ba180bc806cc4081b9ff83c3d3a9d3f9ebe01b6c
jordynhatcher <jordynhatcher@35.40.171.46>
2018-11-28 10:35:37 -0500

    I added the rTC fancy code but I hadn't changed the print statement

--- Lab 6 Part 1/main.c
+++ Lab 6 Part 1/main.c
@@ -4,57 +4,132 @@
 
 /**
  * Jordyn Hatcher
- * Jacob Hatcher
- * Professor Zuidema
+ * Jacob Arenz
  * Final Project
- * Description:
+ * Description: Alarm Clock
  *
- * Cited from Zuidema's and Brakora's  lecture
+ * Cited from Zuidema's and Brakora's lecture
  */
-//void LCD_pins_init(void);
+
+
+void configRTC(void);
+void printRTC(void);
+void month(char* monthStr, uint8_t mon_num);
+void dow(char* dowStr, uint8_t day_num);
+
+// global struct variable called now
+struct
+{
+    uint8_t sec;
+    uint8_t min;
+    uint8_t hour;
+} now;
+
+uint8_t RTC_flag = 0, RTC_alarm;
+
+
+
+
 void LCD_init(void); // makes the cursor blink
+
 void commandWrite(uint8_t command); //writing one byte of command by calling the pushByte() function with the command parameter
 void dataWrite(uint8_t data); //writing one byte of data by calling the pushByte() function with the data parameter
+
 void pushByte(uint8_t byte);    // First pushes the most significant 4 bits of the byte into the data pins by calling the pushNibble() function
 //next, it pushes the least significant 4 bits onto the data pins by calling the pushNibble() function
+
 void pushNibble(uint8_t nibble);    //pushes 1 nibble onto the data pins and pulses the enable pin
+
+
 void PulseEnablePin (void);     //This function will sequence the enable (E) pin
+
+
 void delay_micro(uint32_t microsec);    //SysTick timer generates delay in microseconds
 void delay_milli(uint32_t milli);     //SysTick timer generates delay in milliseconds
+
+
+
+
+
 void SysTick_Init(void); // function that initializes the SysTick Timer
 
 
-
-
-/************************************************************************************************/
 void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;  // stop watchdog timer
-
+    SysTick_Init(); //calls SysTick initialization function
     P4->SEL0 &= ~(BIT0 | BIT2 | BIT4 | BIT5 | BIT6 | BIT7);  //sets port 4 pins for GPIO
     P4->SEL1 &= ~(BIT0 | BIT2 | BIT4 | BIT5 | BIT6 | BIT7);  //sets port 4 pins for GPIO
     P4->DIR |= (BIT0 | BIT2 | BIT4 | BIT5 | BIT6 |BIT7); //makes these pins outputs (port pins P4.4 - P4.7 wired to DB4-DB7)
 
-    //LCD_pins_init(); //intializes LCD port pins
     LCD_init(); //calls this function
-    SysTick_Init(); //calls SysTick initialization function
+    __disable_irq();
+      configRTC();
+      NVIC_EnableIRQ(RTC_C_IRQn);
+      __enable_irq();
 
+      while(1)
+      {
+          if(RTC_flag)
+          {
+              printRTC();
+              RTC_flag = 0;
+          }
+          if(RTC_alarm)
+          {
+              printf("ALARM\n");
+              RTC_alarm = 0;
+          }
+      }
 
-    while(1) //keeps going through the function
-    {
-
-    }
 }
-/************************************************************************************************/
+
+void configRTC(void)
+{
+    RTC_C->CTL0     =   0xA500;     //Write Code, IE on RTC Ready
+    RTC_C->CTL13    =   0x0000;
+    RTC_C->TIM0     = 30<<8 | 55;
+    RTC_C->TIM1     = 2<<8 | 12;
+    RTC_C->DATE     = 11<<8 | 26;
+    RTC_C->YEAR     = 2018;
+    RTC_C->PS1CTL   = 0b11010;
+
+    RTC_C->AMINHR   = 12<<8 | 31 | BIT(15) | BIT(7);
+    RTC_C->ADOWDAY = 0;
+    RTC_C->CTL0     = ((0xA500) | BIT5);
+
+}
+
+void printRTC(void)
+{
+//    char monthStr[5];
+//    char dowStr[5];
+//
+//    month(monthStr, now.mon);
+//    dow(dowStr, now.dow);
 
 
-//void LCD_pins_init()
-//{
-//    P4->SEL0 &= ~(BIT0 | BIT2 | BIT4 | BIT5 | BIT6 | BIT7);  //sets port 4 pins for GPIO
-//    P4->SEL1 &= ~(BIT0 | BIT2 | BIT4 | BIT5 | BIT6 | BIT7);  //sets port 4 pins for GPIO
-//    P4->DIR |= (BIT0 | BIT2 | BIT4 | BIT5 | BIT6 |BIT7); //makes these pins outputs (port pins P4.4 - P4.7 wired to DB4-DB7)
-//}
 
+    printf("%02d:%02d:%02d\n",now.hour, now.min, now.sec);
+
+}
+
+void RTC_C_IRQHandler(void)
+{
+    if(RTC_C->CTL0 & BIT1) {
+        RTC_alarm = 1;
+        RTC_C->CTL0 = 0xA500;
+    }
+    if(RTC_C->PS1CTL & BIT0) {
+        now.sec         =   RTC_C->TIM0>>0 & 0x00FF;
+        now.min         =   RTC_C->TIM0>>8 & 0x00FF;
+        now.hour        =   RTC_C->TIM1>>0 & 0x00FF;
+
+        RTC_flag = 1;
+        RTC_C->PS1CTL &= ~BIT0;
+    }
+
+}
 
 void  LCD_init() //this function makes the cursor blink
     {
@@ -150,4 +225,3 @@ void delay_milli(uint32_t milli)
     SysTick->VAL  = 0;   //Clear the current value in the timer.
     while((SysTick->CTRL & 0x00010000) == 0); //Set the counter flag.
 }
-
